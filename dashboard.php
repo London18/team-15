07c40d38c576d3c422c74367a4a906bdd9c4b0b9
PolymorphMT <marktyler1997@gmail.com>
2018-11-10 04:07:55 +0000

    Du hast

--- dashboard.php
+++ dashboard.php
@@ -14,12 +14,12 @@ $('.modal').modal();
 </script>
 
 <div class="container z-depth-5" id="dash">
-    <h1 id="demo"></h1>
+    <h1 id="date"></h1>
     <div class="row">
-        <div class="col s12 m6">
-            <div class="card blue-grey darken-1">
+        <div class="col s12 m12">
+            <div class="card blue-grey darken-1" id="dashcard">
                 <div class="card-content white-text">
-                    <span class="card-title">15:00-16:00 10/11/18</span>
+                    <span class="card-title">15:00-16:00 </span>
                     <table>
                         <thead>
                         <tr>
@@ -40,12 +40,12 @@ $('.modal').modal();
                 <div class="card-action">
                     <!-- Modal Triggers -->
 
-                    <a class="waves-effect waves-light btn modal-trigger" href="#CheckOut">CheckOut</a>
+                    <a class="waves-effect waves-light btn modal-trigger" href="#CheckOut">Check Out</a>
                     <a class="waves-effect waves-light btn modal-trigger" href="#HomeSafe">Home Safe</a>
 
                     <div id="CheckOut" class="modal">
                         <div class="modal-content">
-                            <h4>Check out</h4>
+                            <h4>Check Out</h4>
                             <p>please add buttons kanya</p>
                         </div>
                         <div class="modal-footer">
@@ -71,7 +71,7 @@ $('.modal').modal();
 <script>
     var d = new Date();
     var n = d.toLocaleDateString()
-    document.getElementById("demo").innerHTML = n;
+    document.getElementById("date").innerHTML = n;
 </script>
 </body>
 </html>
