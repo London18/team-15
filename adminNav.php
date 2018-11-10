69b8f0e501110bbaea460178a660b21b3ff31e56
Dean MacLeod <rukiax@gmail.com>
2018-11-10 01:25:51 +0000

    Merge branch 'markflo' of https://github.com/London18/team-15 into adminBranch
    # Conflicts:
    #	css/mystyle.css
    
    login fixed #howDidIBreakThat

--- adminNav.php
+++ adminNav.php
@@ -1,5 +1,4 @@
 
-<body class="hide-sidedrawer">
 <script>
     jQuery(function($) {
         var $bodyEl = $('body'),
@@ -39,7 +38,7 @@
 </head>
 
 <div id="sidedrawer" class="mui--no-user-select">
-    <div id="sidedrawer-brand" class="mui--appbar-line-height">
+    <div id="sidedrawer-brand" height="200px;" class="mui--appbar-line-height">
         <span><img width="100%" src="images/logo.jpeg" alt="logo" /></span>
     </div>
     <div class="mui-divider"></div>
@@ -62,8 +61,13 @@
 <header id="header">
     <div class="mui-appbar mui--appbar-line-height">
         <div class="mui-container-fluid">
-            <a id="pulloutbar" class="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer"><i id="navpull" class="material-icons">menu</i></a>
+            <a id="pulloutbar" class="sidedrawer-toggle mui--hidden-xs mui--hidden-sm js-hide-sidedrawer"><i id="navpull" class="medium material-icons">menu</i></a>
             <span class="mui--text-title mui--visible-xs-inline-block">Admin</span>
         </div>
     </div>
 </header>
+
+
+<body class="hide-sidedrawer">
+
+<div class="divider"></div>
