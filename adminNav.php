0942ca5ac60f789db75357fb826110c28141bcf2
Dean MacLeod <rukiax@gmail.com>
2018-11-10 00:02:22 +0000

    changes admin.php to timetable

--- adminNav.php
+++ adminNav.php
@@ -1,13 +1,3 @@
-<head>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="css/mystyle.css">
-
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <script src="css/jquery-3.3.1.min.js"></script>
 
 <body class="hide-sidedrawer">
 <script>
