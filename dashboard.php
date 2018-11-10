409c8c9c516232fd10053e33c893a01df3963c23
PolymorphMT <marktyler1997@gmail.com>
2018-11-10 03:05:47 +0000

    Constructing the dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Dashboard</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-    <link href="css/mystyle.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
+<?php include("header.php");
+include("adminNav.php");?>
 <body>
 
 <div class="container z-depth-5" id="dash">
+    <h1 id="demo"></h1>
     <div class="row">
         <div class="col s12 m6">
             <div class="card blue-grey darken-1">
@@ -41,5 +36,10 @@
         </div>
     </div>
 </div>
+<script>
+    var d = new Date();
+    var n = d.toLocaleDateString()
+    document.getElementById("demo").innerHTML = n;
+</script>
 </body>
 </html>
