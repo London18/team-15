50f86f5bcf8e1568cfca147d4792520e861acf7a
Dean MacLeod <rukiax@gmail.com>
2018-11-10 00:55:42 +0000

    changes admin.php to timetable

--- timetable.php
+++ timetable.php
@@ -7,7 +7,35 @@ include('header.php');
 include('adminNav.php');
 ?>
 <div id="content-wrapper">
-    <!-- Main content goes here -->
+
+    <table class="highlight">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Item Name</th>
+            <th>Item Price</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+        </tr>
+        <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+        </tr>
+        <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+        </tr>
+        </tbody>
+    </table>
+
 </div>
 </body>
 <!--JavaScript at end of body for optimized loading-->
