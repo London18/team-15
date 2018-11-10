b0418b56f98c34d0b309f32fc60ce638e4ed01a6
Dean MacLeod <rukiax@gmail.com>
2018-11-10 02:16:21 +0000

    #noticemesenpai

---
+++ include/core.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rukia
+ * Date: 11/9/2018
+ * Time: 9:24 PM
+ */
+
+include_once ($_SERVER['DOCUMENT_ROOT'] .'/include/functions.php');
+include_once ($_SERVER['DOCUMENT_ROOT'] .'/include/config.php');
+spl_autoload_register('classAutoLoad');
